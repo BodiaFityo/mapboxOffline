068e272e3e6380b4b74569d80eb7cb5295eb5515
Bohdan Fito <bohdan.fito@globallogic.com>
2024-05-10 17:11:49 +0300

    update 3d map with styles

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get("/", async (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(8080, () => {
-    console.log("Server successfully running on port 8080");
+app.listen(8081, () => {
+    console.log("Server successfully running on port 8081");
 });
